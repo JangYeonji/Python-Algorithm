d77ebaea9a9284b8b068c3f24f6b9bcc0df934d0
jangyeonji <yunji0220@naver.com>
2021-10-13 10:33:53 +0900

    third commit

--- pgm(L2)_주식가격.py
+++ pgm(L2)_주식가격.py
@@ -1,19 +1,17 @@
-#1차시도 : 2,3,4,5,6,7,8,9,10 & 효율성테스트 실패
+#2차시도 : 성공
 
 def solution(prices):
     stack = [0]   
     answer = [0] * len(prices)
     
     for i in range(1,len(prices)):
-        if prices[i] < prices[stack[-1]]:  
+        if prices[i] < prices[stack[-1]]:   
             for j in stack[::-1]:   
                 if prices[i] < prices[j]:   
-                    answer[j] = i-j  
+                    answer[j] = i-j 
                     stack.pop()
         stack.append(i)
 
-    for i in stack:  
+    for i in stack: 
         answer[i] = len(prices) - (i+1)
     return answer
-
-#prices = [1,2,3,2,3,1], answer = [5,4,1,2,1,0] 으로 고쳐서 해보기
