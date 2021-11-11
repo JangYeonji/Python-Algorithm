98b4466d17c5016c1f727c32e372c774c0ef244f
JangYeonji <yunji0220@naver.com>
2021-11-11 09:44:23 +0900

    first commit

---
+++ pgm(L3)_N으로 표현.py
@@ -0,0 +1,22 @@
+answer = -1
+
+def DFS(n, pos, num, number, s):
+    global answer
+    if pos > 8:
+        return
+    if num == number:
+        if pos < answer or answer == -1:
+            answer=pos
+        return
+
+    nn=0
+    for i in range(8):
+        nn=nn*10+n
+        DFS(n, pos+1+i, num+nn, number, s+'+')
+        DFS(n, pos+1+i, num-nn, number, s+'-')
+        DFS(n, pos+1+i, num*nn, number, s+'*')
+        DFS(n, pos+1+i, num/nn, number, s+'/')
+
+def solution(N, number):    
+    DFS(N, 0, 0, number, '')    
+    return answer
