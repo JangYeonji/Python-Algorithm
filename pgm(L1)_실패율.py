4d582d76a2a57ba9c305ba76b10408c9c7248498
JangYeonji <yunji0220@naver.com>
2021-11-18 12:05:26 +0900

    second commit

--- pgm(L1)_실패율.py
+++ pgm(L1)_실패율.py
@@ -1,11 +1,14 @@
-#1차시도 실패. 1,6,7,9,13,23,24,25
+#성공
 def solution(N, stages):
     fail = {}
     for i in range(1,N+1):
         fail[i] = 0
     total = 0
     for i in range(N):
-        total += stages.count(i)
-        fail[i+1] = stages.count(i+1)/(len(stages)-total)
+        try:
+            total += stages.count(i)
+            fail[i+1] = stages.count(i+1)/(len(stages)-total)
+        except:
+            fail[i+1] = 0
     result = dict(sorted(fail.items(), key=lambda x:-x[1]))
     return list(result.keys())
