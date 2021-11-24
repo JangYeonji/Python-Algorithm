2630a5f6344dabe6ff1f7fe22f049d092bfc936d
JangYeonji <yunji0220@naver.com>
2021-11-24 15:51:59 +0900

    second commit

--- Book(382)_숨바꼭질.py
+++ Book(382)_숨바꼭질.py
@@ -1,4 +1,4 @@
-'''
+'''입력 예시
 6 7
 3 6
 4 3
@@ -9,22 +9,36 @@
 5 2
 '''
 
-import sys
-input = sys.stdin.readline
+import heapq
 n,m = map(int,input().split())
-INF = int(1e9)
-graph = [[INF]*(n+1) for _ in range(n+1)]
-
-for a in range(1,n+1):
-    for b in range(1,n+1):
-        if a==b:
-            graph[a][b] = 0
-
+graph = [[] for _ in range(n+1)]
 for i in range(m):
     a,b = map(int,input().split())
-    graph[a][b] = 1
+    graph[a].append((b,1))
+    graph[b].append((a,1))
 
-for k in range(1,n+1):
-    for a in range(1,n+1):
-        for b in range(1,n+1):
-            graph[a][b] = min(graph[a][b], graph[a][k]+graph[k][b])
+distance = [int(1e9)] * (n+1)
+def dijkstra(start):
+    q = []
+    heapq.heappush(q, (0,start))
+    distance[start] = 0
+    while q:
+        dist,now = heapq.heappop(q)
+        if distance[now]<dist:
+            continue
+        for i in graph[now]:
+            cost = dist+i[1]
+            if cost<distance[i[0]]:
+                distance[i[0]] = cost
+                heapq.heappush(q, (cost,i[0]))
+
+result = [0]*3
+
+result[1] = max(distance[1:])
+result[2] = distance.count(result[1])
+for i in range(len(distance)):
+    if distance[i]==result[1]:
+        result[0] = i
+        break
+for i in result:
+    print(i, end=' ')
