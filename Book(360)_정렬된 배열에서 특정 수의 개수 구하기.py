ecff8495a089aae1b114cb11cf3aa0ea8782bf39
JangYeonji <yunji0220@naver.com>
2021-11-25 22:52:29 +0900

    second commit

--- Book(360)_정렬된 배열에서 특정 수의 개수 구하기.py
+++ Book(360)_정렬된 배열에서 특정 수의 개수 구하기.py
@@ -1,32 +1,21 @@
-'''
+'''입력 예시
 7 2
-1 1 2 2 2 2 3 
+1 1 2 2 2 2 3
 
 7 4
 1 1 2 2 2 2 3
 '''
 
-import sys
-input_data = sys.stdin.readline().rstrip()
-n = int(input_data.split()[0])
-x = int(input_data.split()[1])
-array = list(map(int,input_data.split()[2:]))
+from bisect import bisect_left, bisect_right
+n,x = map(int, input().split())
+num = list(map(int,input().split()))
 
-def solution(start, end, x, array):
-    cnt = 0
-    while(start <= end):
-        mid = int((start + (end - 1)) // 2)
-        if array[mid]==x:
-            cnt += 1
-            del array[mid]
-            end -= 1
-        elif array[mid]>x:
-            end = mid-1
-        elif array[mid]<x:
-            start = mid+1
-    if cnt==0:
-        return -1
-    else:
-        return cnt
+left = bisect_left(num,x)
+right = bisect_right(num,x)
 
-solution(0,n,x,array)
+result = right-left
+
+if result==0:
+    print(-1)
+else:
+    print(result)
