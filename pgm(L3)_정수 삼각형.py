3ff30d7f5872c17e778182169dde89218e8fcf9b
JangYeonji <yunji0220@naver.com>
2021-11-26 21:54:58 +0900

    second commit

--- pgm(L3)_정수 삼각형.py
+++ pgm(L3)_정수 삼각형.py
@@ -1,36 +1,13 @@
-#ㄳ1
-solution = lambda t, l = []: max(l) if not t else solution(t[1:], [max(x,y)+z for x,y,z in zip([0]+l, l+[0], t[0])])
-
-#ㄳ2
 def solution(triangle):
-    dp = []
-    for t in range(1, len(triangle)):
-        for i in range(t+1):
-            if i == 0:
-                triangle[t][0] += triangle[t-1][0]
-            elif i == t:
-                triangle[t][-1] += triangle[t-1][-1]
+    for i in range(len(triangle)):
+        for j in range(len(triangle[i])):
+            if len(triangle[i])==1:
+                continue
+            if j==0:
+                triangle[i][j] += triangle[i-1][j]
+            elif j==len(triangle[i])-1:
+                triangle[i][j] += triangle[i-1][-1]
             else:
-                triangle[t][i] += max(triangle[t-1][i-1], triangle[t-1][i])
+                triangle[i][j] += max(triangle[i-1][j-1],triangle[i-1][j])
+    
     return max(triangle[-1])
-
-#ㄳ3
-def solution(triangle):
-    memo = {}
-    answer = f(triangle, 0, 0, memo)
-    return answer
-
-def f(triangle, i, j, memo):
-    if i == len(triangle)-1:
-        return triangle[i][j]
-
-    if (i,j) in memo:
-        return memo[(i,j)]
-
-    a = f(triangle, i+1, j, memo)
-    b = f(triangle, i+1, j+1, memo)
-    x = triangle[i][j] + max(a, b)
-
-    memo[(i,j)] = x
-
-    return x
