b09d17acac2c7168ae125e49850d0f83daf5f9cb
JangYeonji <yunji0220@naver.com>
2021-11-29 16:27:00 +0900

    second commit

--- pgm(L2)_프린터.py
+++ pgm(L2)_프린터.py
@@ -1,4 +1,5 @@
-#1차 시도. 2,5,18 실패.
+#while 문에서 만약 q에서 꺼낸 원소가 마지막이었을 경우를 고려
+
 def solution(priorities, location):
     q = []
     count = 0
@@ -6,6 +7,9 @@ def solution(priorities, location):
         q.append((idx,i))
     while q:
         tmp = q.pop(0)
+        if q==[]:
+            count += 1
+            break
         if tmp[1] < max(q ,key=lambda x:x[1])[1]:
             q.append(tmp)
         else:
