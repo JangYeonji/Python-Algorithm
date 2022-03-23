7d2dd3b053e1c078b33652358118060e87ae6185
JangYeonji <yunji0220@naver.com>
2022-03-23 14:28:43 +0900

    음얼먹

--- Book(146)_음료수 얼려 먹기.py
+++ Book(146)_음료수 얼려 먹기.py
@@ -17,30 +17,35 @@
 11100011111111
 '''
 
-import sys
-input_data = sys.stdin.readline().rstrip()
-n = int(input_data.split()[0])
-m = int(input_data.split()[1])
+from collections import deque
 
+n,m = map(int,input().split())
 graph = []
-for i in range(2,n+2):
-    graph.append(list(map(int,input_data.split()[i])))
+for i in range(n):
+    graph.append(list(input()))
 
-def dfs(x,y):
-    if x<=-1 or x>=n or y<=-1 or y>=m:
-        return False
-    if graph[x][y] == 0:
-        graph[x][y] == 1
-        dfs(x-1, y)
-        dfs(x,y-1)
-        dfs(x+1,y)
-        dfs(x,y+1)
-        return True
-    return False
+def dfs(x,y,graph,n,m):
+    dx = [-1,1,0,0]
+    dy = [0,0,-1,1]
+    q = deque([(x,y)])
+    graph[x][y] = '1'
+    while q:
+        xx,yy = q.popleft()
+        for i in range(4):
+            nx = xx+dx[i]
+            ny = yy+dy[i]
+            if nx>=0 and ny>=0 and nx<n and ny<m:
+                if graph[nx][ny] == '0':
+                    graph[nx][ny] = '1'
+                    q.append((nx,ny))
+    return graph
 
-result = 0
+
+answer = 0
 for i in range(n):
     for j in range(m):
-        if dfs(i,j)==True:
-            result += 1
-print(result)
+        if graph[i][j]=='0':
+            graph = dfs(i,j,graph,n,m)
+            answer += 1
+            
+print(answer)
