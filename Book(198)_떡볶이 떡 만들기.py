f413ffd82c39e67adcba8b25677f08e1a1c6ac32
JangYeonji <yunji0220@naver.com>
2022-03-29 16:16:27 +0900

    떡

--- Book(198)_떡볶이 떡 만들기.py
+++ Book(198)_떡볶이 떡 만들기.py
@@ -1,28 +1,24 @@
-'''
-4 6
-19 15 10 17
-'''
+n,m = map(int,input().split())
+rc = list(map(int,input().split()))
+rc.sort()
 
-import sys
-input_data = sys.stdin.readline().rstrip()
+left = rc[0]
+right = rc[-1]
 
-m = int(input_data.split()[1])
-rice_cake = list(map(int,input_data.split()[2:]))
-def solution(m,rice_cake):
-    start = 0
-    end = max(rice_cake)
-    while(start<=end):
-        mid = int((start + end) // 2)
-        rice_sum = 0
-        for i in rice_cake:
-            if i>mid:
-                rice_sum += i-mid
-        if rice_sum == m:
-            return mid
-        elif rice_sum>m:
-            start = mid+1
-        elif rice_sum<m:
-            end = mid-1
-    return mid
-
-solution(m,rice_cake)
+while left<right:
+    if (left+right)//2 != mid:
+        mid = (left+right)//2
+    else:
+        print(mid)
+        break
+    tmp = 0
+    for i in rc:
+        if i>mid:
+            tmp += i - mid
+    if tmp==m:
+        print(mid)
+        break
+    elif tmp<m:
+        right = mid
+    elif tmp>m:
+        left = mid
